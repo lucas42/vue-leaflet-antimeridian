4df114ca6bb0868bbfeb906ffdac5e53dd4ddbfa
Luke Blaney <git@lukeblaney.co.uk>
2025-08-02 17:58:10 +0100

    use global version of leaflet library, to reduce file size and make instanceof comparisions work properly when imported into a larger app

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import vue from '@vitejs/plugin-vue2'
 
 export default {
-	plugins: [vue()],
-	build: {
-		lib: {
-			entry: 'index.js',
-			name: 'vue-leaflet-antimeridian',
-			fileName: 'vue-leaflet-antimeridian',
-		},
-	},
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: 'index.js',
+      name: 'vue-leaflet-antimeridian',
+      fileName: 'vue-leaflet-antimeridian',
+    },
+    rollupOptions: {
+      external: ['leaflet'],
+      output: {
+        globals: {
+          leaflet: 'L',
+        },
+      },
+    },
+  },
 }
